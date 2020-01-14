8e7679e8e28224dd1d55a32803ed28ff07ea0157
KristiinaCwalina <kristiina.cwalina@gmail.com>
2020-01-14 19:31:39 +0100

    modified js

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // api key : 82005d27a116c2880c8f0fcb866998a0
+function getLocation() {
+  //console.log(navigator.geolocation);
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition, showError);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  //console.log(position);
+}
+function showError(error) {
+  console.log(error);
+}
+getLocation();
