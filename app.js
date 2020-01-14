e4424146259e1281d0bd725c0b13e5418d84d145
KristiinaCwalina <kristiina.cwalina@gmail.com>
2020-01-14 21:10:14 +0100

    lat long

--- app.js
+++ app.js
@@ -1,17 +1,40 @@
-// api key : 82005d27a116c2880c8f0fcb866998a0
+const apikey = "fc23f3d15fe82766be499abecf9ba733";
+
+let latitude;
+let longitude;
+const notification = document.getElementsByClassName("notification")[0];
+
 function getLocation() {
   //console.log(navigator.geolocation);
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition, showError);
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
   } else {
     x.innerHTML = "Geolocation is not supported by this browser.";
   }
 }
 
-function showPosition(position) {
+function onSuccess(position) {
+  latitude = position.coords.latitude;
+  longitude = position.coords.longitude;
+  const weather = fetch(
+    "https://api.openweathermap.org/data/2.5/weather?" +
+      "lat=" +
+      latitude +
+      "&lon=" +
+      longitude +
+      "&appid=" +
+      apikey
+  );
+  console.log("info", weather);
   //console.log(position);
+  //latitude=position.coords.
 }
-function showError(error) {
-  console.log(error);
+function onError(error) {
+  console.error(error);
+  //notification[0].innerHTML = error.message;
+  const p = document.createElement("p");
+  p.innerHTML = error.message;
+  notification.style.display = "block";
+  notification.appendChild(p);
 }
 getLocation();
