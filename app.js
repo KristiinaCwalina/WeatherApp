125d5631b82c8c09d385b620e37a5c4b0330777d
KristiinaCwalina <kristiina.cwalina@gmail.com>
2020-01-18 10:52:11 +0100

    innerHTML

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ function getLocation() {
     notification.innerHTML = "Geolocation is not supported by this browser.";
   }
 }
+function kelvinToCelsius(temp) {
+  return temp - 273.15;
+}
 
 function onSuccess(position) {
   latitude = position.coords.latitude;
@@ -28,11 +31,26 @@ function onSuccess(position) {
   weather
     .then((response) => response.json())
     .then((weather) => {
-      console.log(weather);
-      console.log(weather.weather[0].icon);
-      console.log(parseInt(weather.main.temp - 273.15));
-      console.log(weather.weather[0].main);
-      console.log(weather.name);
+      //console.log(weather);
+      //console.log(weather.weather[0].icon);
+      //console.log(parseInt(weather.main.temp - 273.15));
+      //console.log(weather.weather[0].main);
+      //console.log(weather.name);
+
+      const location = weather.name;
+      const loacationHtmlElement = document.getElementsByClassName("location")[0];
+      loacationHtmlElement.innerHTML = "<p>" + location + "</p>";
+      const temp = kelvinToCelsius(weather.main.temp).toFixed(1);
+      const tempHtmlElement = document.getElementsByClassName("temperature-value")[0];
+      tempHtmlElement.innerHTML = "<p>" + temp + "°<span>C</span></p>";
+      const icon = weather.weather[0].icon;
+      const iconHtmlElement = document.getElementsByClassName("weather-icon")[0];
+      iconHtmlElement.innerHTML = "<img src='icons/" + icon + ".png' alt=''></img>";
+      const weatherDescription = weather.weather[0].main;
+      const weatherDescriptionHtmlElement = document.getElementsByClassName(
+        "temperature-description"
+      )[0];
+      weatherDescriptionHtmlElement.innerHTML = "<p>" + weatherDescription + "</p>";
     });
 }
 //console.log("info", weather);
